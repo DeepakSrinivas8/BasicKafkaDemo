b97d2ac60335342be96fca40f004f45188829b19
Deepak.Ganapathi <ganapathideepak97@gmail.com>
2024-02-02 19:43:43 +0530

    Basic Kafka Demo

---
+++ cab-book-user/src/main/java/com/poc/cabbookuser/service/CabLocationService.java
@@ -0,0 +1,18 @@
+package com.poc.cabbookuser.service;
+
+import com.poc.cabbookuser.constant.AppConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CabLocationService {
+
+    @Autowired
+    KafkaTemplate<String,Object> kafkaTemplate;
+
+    public boolean updateLocation(String location){
+        kafkaTemplate.send(AppConstants.CAB_LOCATION,location);
+        return true;
+    }
+}
